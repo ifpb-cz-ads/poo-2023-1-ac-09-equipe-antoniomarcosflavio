85d2409d057d0d80facb8776c203b1d9a60a42bf
AntonioMarcosDev <ambrcz@hotmail.com>
2023-05-10 20:16:38 -0300

    Adicionando CTExercicio01 e CTExercicio02

---
+++ ContaPoupanca.java
@@ -0,0 +1,20 @@
+public class ContaPoupanca extends Conta{
+
+    public ContaPoupanca(int numero, Cliente titular) {
+        super(numero, titular);
+    }
+
+    public void reajustar(double percentual) {
+        this.saldo = this.saldo + this.saldo * percentual;
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        if (this.getSaldo() >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
